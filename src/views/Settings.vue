35ae546db9530904387592a93bc313273b7db1f8
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-21 13:55:11 +0300

    Enhanced layout

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="mt-6">
+  <v-container class="mt-6 px-6 px-sm-10">
     <v-row class="text-h4">
       Settings
     </v-row>
@@ -7,29 +7,29 @@
       Define your bearer token here, it will be saved to local storage for later
       use
     </v-row>
-    <v-row class="mt-8">
-      <v-col cols="10" class="pr-4">
+    <v-row class="mt-8 flex-nowrap">
+      <div class="pr-2 flex-grow-1">
         <v-text-field
           label="Token"
           outlined
           v-model="tokenFieldValue"
           type="password"
         ></v-text-field>
-      </v-col>
-      <v-col cols="2" class="pl-4">
+      </div>
+      <div class="pl-2">
         <v-btn
           color="primary"
           height="56"
-          block
-          class="text-none text-subtitle-1"
+          width="100"
+          class="text-none text-subtitle-1 flex-shrink-1"
           @click="setBearerToken"
         >
           Set
         </v-btn>
-      </v-col>
+      </div>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col class="px-0">
         <v-fade-transition>
           <v-alert :type="alert.type" v-show="alert.show">
             {{ alert.content }}
