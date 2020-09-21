6aa73dcbf29f3a20b19727ee8a63799869478066
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-21 13:44:55 +0300

    Implemented token adding and success / failure message

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,18 +1,19 @@
 <template>
-  <v-container class="my-2">
+  <v-container class="mt-6">
     <v-row class="text-h4">
       Settings
     </v-row>
-    <v-row class="text-subtitle-1">
+    <v-row class="text-subtitle-1 mt-1">
       Define your bearer token here, it will be saved to local storage for later
       use
     </v-row>
-    <v-row class="mt-10">
+    <v-row class="mt-8">
       <v-col cols="10" class="pr-4">
         <v-text-field
           label="Token"
           outlined
           v-model="tokenFieldValue"
+          type="password"
         ></v-text-field>
       </v-col>
       <v-col cols="2" class="pl-4">
@@ -30,8 +31,8 @@
     <v-row>
       <v-col>
         <v-fade-transition>
-          <v-alert type="success" v-show="showAlert">
-            I'm a success alert.
+          <v-alert :type="alert.type" v-show="alert.show">
+            {{ alert.content }}
           </v-alert>
         </v-fade-transition>
       </v-col>
@@ -45,13 +46,38 @@ export default {
   data() {
     return {
       tokenFieldValue: '',
-      showAlert: false
+      alert: {
+        show: false,
+        type: 'success',
+        content: ''
+      }
     }
   },
   methods: {
+    showAlert({ type, content }) {
+      this.alert = { ...this.alert, show: true, type, content }
+    },
     setBearerToken() {
-      console.log(this.tokenFieldValue)
+      if (!this.tokenFieldValue) {
+        this.showAlert({ type: 'error', content: 'Enter value' })
+        return
+      }
+      if (!('localStorage' in window)) {
+        this.showAlert({ type: 'error', content: 'Localstorage not available' })
+        return
+      }
+      const currentToken = localStorage.getItem('token')
+      if (currentToken) localStorage.removeItem('token')
+      localStorage.setItem('token', this.tokenFieldValue)
+      this.showAlert({
+        type: 'success',
+        content: 'Succesfully added to local storage'
+      })
     }
+  },
+  mounted() {
+    if (window.localStorage && localStorage.getItem('token'))
+      this.tokenFieldValue = localStorage.getItem('token')
   }
 }
 </script>
