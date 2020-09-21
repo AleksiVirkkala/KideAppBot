92c1d5cc7ae1fb0033d4e8e18d56efe088ae69d2
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-21 15:17:11 +0300

    Added logging methods to display progress to user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,35 +16,43 @@
         </v-alert>
       </v-col>
     </v-row>
-    <v-row class="mt-10 flex-nowrap">
-      <div class="pr-2 flex-grow-1">
-        <v-text-field
-          label="Event URL"
-          outlined
-          :disabled="promtToAddToken"
-        ></v-text-field>
-      </div>
-      <div class="pl-2">
-        <v-btn
-          color="primary"
-          height="56"
-          width="100"
-          class="text-none text-subtitle-1 flex-shrink-1"
-          :disabled="promtToAddToken"
-          elevation="0"
-        >
-          Activate
-        </v-btn>
-      </div>
-    </v-row>
-    <v-row>
-      <v-col class="px-0">
-        <v-fade-transition>
-          <v-alert :type="alert.type" v-show="alert.show">
-            {{ alert.content }}
-          </v-alert>
-        </v-fade-transition>
-      </v-col>
+    <v-form ref="urlField">
+      <v-row class="mt-10 flex-nowrap">
+        <div class="pr-2 flex-grow-1">
+          <v-text-field
+            label="Event URL"
+            v-model="eventUrl"
+            outlined
+            validate-on-blur
+            :disabled="promtToAddToken || this.botIsActive"
+            :rules="[(value) => !!value || 'Enter value']"
+          ></v-text-field>
+        </div>
+        <div class="pl-2">
+          <v-btn
+            color="primary"
+            height="56"
+            width="100"
+            class="text-none text-subtitle-1 flex-shrink-1"
+            :disabled="promtToAddToken || this.botIsActive"
+            elevation="0"
+            @click="startBot"
+          >
+            Activate
+          </v-btn>
+        </div>
+      </v-row>
+    </v-form>
+    <v-row class="mt-4">
+      <v-textarea
+        filled
+        name="ProgressLog"
+        label="Progress log"
+        v-model="logValue"
+        auto-grow
+        outlined
+        readonly
+      ></v-textarea>
     </v-row>
   </v-container>
 </template>
@@ -55,11 +63,47 @@ export default {
   data() {
     return {
       promtToAddToken: false,
-      alert: {
-        show: false,
-        type: 'success',
-        content: ''
+      eventUrl: '',
+      logValue: '',
+      botIsActive: false
+    }
+  },
+  methods: {
+    log(text = '', type, replace) {
+      let prefix = this.logValue ? '\n' : ''
+      if (type === 'e') prefix += '❌ '
+      else if (type === 'w') prefix += '⚠️ '
+      else if (type === 's') prefix += '✅ '
+      else if (type === 'l') prefix += '⌛ '
+
+      if (replace) {
+        this.logValue = this.logValue.substr(0, this.logValue.lastIndexOf('\n'))
       }
+
+      this.logValue += prefix + text
+    },
+    logSeparation() {
+      if (this.logValue) {
+        this.log()
+        this.log('----------')
+        this.log()
+      }
+    },
+    stopBot(msg, type) {
+      this.log(msg, type)
+      this.botIsActive = false
+    },
+    async startBot() {
+      if (!this.$refs.urlField.validate()) return
+      this.botIsActive = true
+      this.logSeparation()
+      const url = this.eventUrl
+      const idStartIndex = url.lastIndexOf('/') + 1
+      if (idStartIndex === 0 || idStartIndex === url.length) {
+        this.log('sadf')
+        this.stopBot("Couldn't parse productPageId from given URL", 'e')
+      }
+      // const productPageId = url.substr(url.lastIndexOf('/') + 1, url.length)
     }
   },
   created() {
