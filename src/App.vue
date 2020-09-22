6ecee965a00a287a08dc3ab30d5c5e5832bcadee
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-23 00:32:03 +0300

    Fixed body scrolling on ios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
     <Header />
-    <v-main>
-      <div style="height: 100%; width: 100%;">
-        <router-view></router-view>
-      </div>
+    <v-main
+      ref="main"
+      style="-webkit-overflow-scrolling: touch; height: 100vh; overflow: scroll;"
+    >
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import { disableBodyScroll } from 'body-scroll-lock'
+
 export default {
   name: 'App',
 
@@ -18,6 +21,9 @@ export default {
     Header
   },
 
-  data: () => ({})
+  data: () => ({}),
+  mounted() {
+    disableBodyScroll(this.$refs.main.$el)
+  }
 }
 </script>
