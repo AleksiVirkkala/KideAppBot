ac7449522d95388ddbb0086a91aa35db4f31410d
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-23 02:24:59 +0300

    Implemented toolbar to position properly based on safe area (notch). Not elegant solution but works.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,60 @@
 <template>
-  <v-app-bar app color="primary" class="px-1" dark>
-    <div class="d-flex align-center">
-      <span class="text-h6">Kide.app ticket bot</span>
+  <v-app-bar
+    flat
+    absolute
+    app
+    color="primary"
+    class="px-1"
+    dark
+    v-resize="reSetSafeArea"
+    :height="56 + safeAreaTop"
+  >
+    <div class="d-flex fill-height flex-grow-1 align-end mx-n4 mx-sm-n2">
+      <div class="d-flex flex-grow-1">
+        <v-avatar height="40" class="mt-1">
+          <v-img src="@/assets/logoonlyletters.png"> </v-img>
+        </v-avatar>
+        <div class="d-flex align-center">
+          <span class="text-h6">Kide.app ticket bot</span>
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list width="164">
+            <v-list-item-group color="primary">
+              <v-list-item :to="{ name: 'Home' }" exact>
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>Home</v-list-item-content>
+              </v-list-item>
+              <v-list-item :to="{ name: 'Settings' }">
+                <v-list-item-icon>
+                  <v-icon>mdi-cog</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>Settings</v-list-item-content>
+              </v-list-item>
+              <v-list-item
+                href="https://github.com/AleksiVirkkala/KideAppBot"
+                target="_blank"
+              >
+                <v-list-item-icon>
+                  <v-icon>mdi-github</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>GitHub</v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </div>
     </div>
-
-    <v-spacer></v-spacer>
-
-    <v-menu left bottom>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn icon v-bind="attrs" v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list width="164">
-        <v-list-item-group color="primary">
-          <v-list-item :to="{ name: 'Home' }" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>Home</v-list-item-content>
-          </v-list-item>
-          <v-list-item :to="{ name: 'Settings' }">
-            <v-list-item-icon>
-              <v-icon>mdi-cog</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>Settings</v-list-item-content>
-          </v-list-item>
-          <v-list-item
-            href="https://github.com/AleksiVirkkala/KideAppBot"
-            target="_blank"
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-github</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>GitHub</v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-menu>
   </v-app-bar>
 </template>
 
@@ -48,6 +64,25 @@ export default {
 
   components: {},
 
-  data: () => ({})
+  data: () => ({
+    safeAreaTop: 0
+  }),
+  methods: {
+    reSetSafeArea() {
+      this.safeAreaTop = parseInt(
+        getComputedStyle(document.documentElement).getPropertyValue('--sat')
+      )
+    }
+  },
+  mounted() {
+    this.$nextTick(this.reSetSafeArea)
+    console.log(this.safeAreaTop)
+  }
 }
 </script>
+
+<style lang="css">
+:root {
+  --sat: env(safe-area-inset-top);
+}
+</style>
