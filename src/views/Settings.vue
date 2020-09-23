26d3684157511f8c09bd8bb272e799ef4a7b32bc
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-23 14:21:06 +0300

    Updated version number

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -5,7 +5,7 @@
   >
     <v-row class="text-h5" align="center">
       Settings
-      <span class="ml-auto text-body-1 text--disabled">v1.1.0</span>
+      <span class="ml-auto text-body-1 text--disabled">v1.2.0</span>
     </v-row>
     <v-row class="text-subtitle-1 mt-1">
       Define your bearer token here, it will be saved to local storage for later
