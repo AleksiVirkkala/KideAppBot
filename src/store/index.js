ca88e56b7546c6865e8fc4050a184cb1f9a3a433
AleksiVirkkala <aleksi.virkkala@gmail.com>
2020-09-26 13:38:28 +0300

    Added base for vuex state to be used for log rows

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tokenSet: false,
+    logRows: [],
+    juttu: 69
   },
   mutations: {
+    addLogRow(state, row) {
+      state.logRows.push(row)
+    },
+    clearLog(state) {
+      state.logRows = []
+    }
   },
   actions: {
+    teeJuttu(state) {
+      setTimeout(() => {
+        state.juttu = 420
+      }, 2000)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
