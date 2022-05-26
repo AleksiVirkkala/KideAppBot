348743bed99c63ad27d4cd28392f178ae7cdc661
Aleksi Virkkala <aleksi.virkkala@gmail.com>
2022-05-26 14:54:19 +0300

    feat: :construction: create formatter and linter configurations and entry file

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import './registerServiceWorker';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// eslint-disable-next-line
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div>Moi :D</div>
+  </StrictMode>
+);
