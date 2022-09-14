9fdcde9131d6c48b11aa6b6ac29ddfccf85dbd5b
Aleksi Virkkala <aleksi.virkkala@gmail.com>
2022-09-14 10:40:25 +0300

    Temp

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -16,25 +16,18 @@ const Bot = () => {
   const [logData, setLogData] = useState<Log[]>([]);
 
   useEffect(() => {
-    socket.on('logsChanged', logs => {
-      setLogData(logs);
+    socket.on('logsChanged', log => {
+      console.log(log);
+      setLogData(logs => [...logs, log]);
+      // setLogData([...logData, log]);
     });
     socket.on('isRunningChanged', newValue => {
       setIsRunning(newValue);
     });
-
-    void axios.post(
-      'https://api.kide.app/api/reservations',
-      '{"toCreate":[{"inventoryId":"5d78a81f-89fd-4a6f-b6a6-ad444af02e08","quantity":6,"productVariantUserForm":null}],"toCancel":null}',
-      {
-        headers: {
-          accept: 'application/json, text/plain, */*',
-          authorization:
-            'Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl9pZCI6IjhjMTE1NGE0MWQxZDQ3N2M5YTIyMjQzODJiOWJjMmZiIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImFsZWtzaS52aXJra2FsYUB0dW5pLmZpIiwic3ViIjoiYWxla3NpLnZpcmtrYWxhQHR1bmkuZmkiLCJ1c2VyX2lkIjoiY2JjMDJhMzUtY2YzNC00N2ZiLTlmOWUtNjBkNDBhZmJhZTFmIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiMCIsIm5iZiI6MTY1OTg4Njk1OSwiZXhwIjoxNjc1NDM4OTU5LCJpc3MiOiJodHRwczovL2F1dGgua2lkZS5hcHAiLCJhdWQiOiI1NmQ5Y2JlMjJhNTg0MzJiOTdjMjg3ZWFkZGEwNDBkZiJ9.IC8ytFukjADKzusjno01SgvQNin3PmKkN9W9igG7KqTwE7OIf4GUYXiB467xWoahapFW_kB3NTHq-whRlauQMQ',
-          'Content-Type': 'application/json;charset=UTF-8'
-        }
-      }
-    );
+    return () => {
+      socket.off('logsChanged');
+      socket.off('isRunningChanged');
+    };
   }, []);
 
   const runBot = () => {
