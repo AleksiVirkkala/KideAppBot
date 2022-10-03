60a996d937d1b7046b60e593414a43e26b247514
Aleksi Virkkala <aleksi.virkkala@gmail.com>
2022-10-03 09:43:48 +0300

    Configure firebase deployment

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,7 @@
-import { io } from "socket.io-client"
-  
-export const socket = io(import.meta.env.VITE_BACKEND_URL)
+import { io } from 'socket.io-client';
 
-socket.emit('private message', 'hello world')
+export const socket = io(
+  import.meta.env.VITE_BACKEND_URL || 'https://wg.aleksivirkkala.com'
+);
+
+socket.emit('private message', 'hello world');
