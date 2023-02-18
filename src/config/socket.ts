12d1d19987fae399319340e9f4c31a9293b95ba8
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-02-18 10:16:06 +0200

    refactor: :truck: rename backend subdomain to better describe purpose

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 export const socket = io(
-  import.meta.env.VITE_BACKEND_URL || 'https://wg.aleksivirkkala.com'
+  import.meta.env.VITE_BACKEND_URL || 'https://bot.aleksivirkkala.com'
 );
