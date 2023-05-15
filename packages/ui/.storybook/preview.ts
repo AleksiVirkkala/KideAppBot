40ea2fa824ad266596138f599605bca720ad4a70
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-15 18:19:12 +0300

    feat(ui): :iphone: Add more viewport presets to storybook

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,5 +1,23 @@
 import 'tailwind-config/load.css';
 import type { Preview } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  fullHD: {
+    name: 'Full HD',
+    styles: {
+      width: '1920px',
+      height: '1080px'
+    }
+  },
+  highResolution: {
+    name: 'High resolution',
+    styles: {
+      width: '2560px',
+      height: '1440px'
+    }
+  }
+};
 
 const preview: Preview = {
   parameters: {
@@ -12,6 +30,12 @@ const preview: Preview = {
     },
     nextjs: {
       appDirectory: true
+    },
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...customViewports
+      }
     }
   }
 };
