f6f7dc979f8ab027f838828143633b1cb3c27454
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-16 15:26:58 +0300

    feat(ui): :wheelchair: Make ExpandTransition adjust to reduced motion

--- packages/ui/src/helpers/Motion/ExpandTransition.tsx
+++ packages/ui/src/helpers/Motion/ExpandTransition.tsx
@@ -1,26 +1,52 @@
-import { AnimatePresence, motion } from 'framer-motion';
+import {
+  AnimatePresence,
+  AnimatePresenceProps,
+  motion,
+  useReducedMotionConfig,
+  Variants
+} from 'framer-motion';
 import { FC } from 'react';
 
-export const ExpandTransition: FC<{ children: React.ReactNode; expanded: boolean }> = ({
+type Mode = AnimatePresenceProps['mode'];
+
+interface ExpandTransitionProps {
+  children: React.ReactNode;
+  expanded: boolean;
+  mode?: Mode;
+}
+
+export const ExpandTransition: FC<ExpandTransitionProps> = ({
   children,
-  expanded
+  expanded,
+  mode: modeProp
 }) => {
+  const prefersReducedMotion = useReducedMotionConfig();
+  const mode: Mode = modeProp || (prefersReducedMotion ? 'popLayout' : undefined);
+
+  const variants: Variants = {
+    expanded: { opacity: 1, height: 'auto' },
+    collapsed: { opacity: 0, height: 0 }
+  };
+  const reducedMotionVariants: Variants = {
+    expanded: { opacity: 1 },
+    collapsed: { opacity: 0 }
+  };
+
   return (
-    <AnimatePresence>
-      {expanded && (
-        <motion.div
-          className="overflow-hidden"
-          initial="collapsed"
-          animate="expanded"
-          exit="collapsed"
-          variants={{
-            expanded: { opacity: 1, height: 'auto' },
-            collapsed: { opacity: 0, height: 0 }
-          }}
-        >
-          {children}
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <>
+      <AnimatePresence mode={mode}>
+        {expanded && (
+          <motion.div
+            className="overflow-hidden"
+            initial="collapsed"
+            animate="expanded"
+            exit="collapsed"
+            variants={prefersReducedMotion ? reducedMotionVariants : variants}
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
