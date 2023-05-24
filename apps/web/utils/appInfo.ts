48f8d46d0c609765e6855a15e2b98207038f8f92
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-24 11:27:24 +0300

    feat(web): :truck: Define app constants in single file

---
+++ apps/web/utils/appInfo.ts
@@ -0,0 +1,5 @@
+import packageJson from '@/package.json';
+
+export const appName = 'KideAppBot';
+export const appDescription = 'Ticket bot for Kide.app';
+export const appVersion = packageJson.version;
