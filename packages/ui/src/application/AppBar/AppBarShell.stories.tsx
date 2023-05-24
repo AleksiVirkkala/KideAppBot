8d976017da8fdb999d42dc5283d1f11d5dc925c8
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-24 19:55:06 +0300

    fix(ui): :bug: Update AppBarShell storybook to new format

--- packages/ui/src/application/AppBar/AppBarShell.stories.tsx
+++ packages/ui/src/application/AppBar/AppBarShell.stories.tsx
@@ -1,32 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { AppBarContent, AppBarShell } from './AppBarShell';
+import { AppBarContext, AppBarShell } from './AppBarShell';
 import { SampleContent } from '../../helpers/Content';
+import { useContext } from 'react';
 
-const primaryContent: AppBarContent = ({ isExpanded, setIsExpanded }) => (
-  <div className="bg-indigo-200/20 p-4">
-    <button className="w-full rounded-lg border-2 p-4" onClick={() => setIsExpanded(!isExpanded)}>
-      expand
-    </button>
-  </div>
-);
+const PrimaryContent = () => {
+  const { isExpanded, setIsExpanded } = useContext(AppBarContext);
+  return (
+    <div className="bg-indigo-200/20 p-4">
+      <button className="w-full rounded-lg border-2 p-4" onClick={() => setIsExpanded(!isExpanded)}>
+        expand
+      </button>
+    </div>
+  );
+};
 
-const collapsibleContent: AppBarContent = ({ isExpanded, setIsExpanded }) => (
-  <div className="bg-green-200/20 p-4">
-    <button
-      className="h-20 w-full rounded-lg border-2 p-4"
-      onClick={() => setIsExpanded(!isExpanded)}
-    >
-      collapse
-    </button>
-  </div>
-);
+const CollapsibleContent = () => {
+  return (
+    <div className="bg-green-200/20 p-4">
+      <AppBarShell.CloseTrigger>
+        <button className="h-20 w-full rounded-lg border-2 p-4">collapse</button>
+      </AppBarShell.CloseTrigger>
+    </div>
+  );
+};
 
 const meta: Meta<typeof AppBarShell> = {
   component: AppBarShell,
   args: {
     className: 'backdrop-blur-sm',
-    children: primaryContent,
-    collapsibleContent
+    children: <PrimaryContent />,
+    collapsibleContent: <CollapsibleContent />
   },
   decorators: [
     Story => (
