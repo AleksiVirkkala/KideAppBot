72e38333a092e40fd4301d90772317ab6530da6a
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-27 20:59:15 +0300

    feat(web): :sparkles: Use svelte-og to create dynamic images

---
+++ apps/web/src/lib/components/og/AppIcon.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let message = 'Hello, world!';
+</script>
+
+<div>{message}</div>
+
+<style lang="postcss">
+	div {
+		background-color: white;
+		@apply flex h-full w-full items-center justify-center text-7xl opacity-100;
+	}
+</style>
