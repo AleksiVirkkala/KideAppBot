0b919811ab7a81b750740993de401078af216bac
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-27 11:21:23 +0300

    feat(project): :wrench: Configure prettier tailwind plugin for custom methods

--- prettier.config.js
+++ prettier.config.js
@@ -5,5 +5,7 @@ module.exports = {
   trailingComma: 'none',
   printWidth: 100,
   plugins: [require('prettier-plugin-tailwindcss')],
-  pluginSearchDirs: ['.']
+  pluginSearchDirs: false,
+  tailwindFunctions: ['twMerge', 'tw'],
+  tailwindAttributes: ['tw'] // Next.js og uses tw attribute instead of className
 };
