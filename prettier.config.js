9f9be78890443452973364acef0a2f6df63102ca
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-27 11:52:48 +0300

    fix(project): :building_construction: Move global config files to correct places

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  singleQuote: true,
-  semi: true,
-  arrowParens: 'avoid',
-  trailingComma: 'none',
-  printWidth: 100,
-  plugins: [require('prettier-plugin-tailwindcss')],
-  pluginSearchDirs: false,
-  tailwindFunctions: ['twMerge', 'tw'],
-  tailwindAttributes: ['tw'] // Next.js og uses tw attribute instead of className
+	singleQuote: true,
+	semi: true,
+	arrowParens: 'avoid',
+	trailingComma: 'none',
+	printWidth: 100,
+	plugins: [require('prettier-plugin-svelte'), require('prettier-plugin-tailwindcss')],
+	pluginSearchDirs: ['.'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+	tailwindFunctions: ['twMerge', 'tw'],
+	tailwindAttributes: ['tw'] // Next.js og uses tw attribute instead of className
 };
