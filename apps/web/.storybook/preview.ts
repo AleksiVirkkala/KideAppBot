c1774c188a90de8d09e258a52e0f6dba0a52a95f
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-27 14:43:17 +0300

    feat(web:ui): :heavy_plus_sign: Add more viewports to storybook

--- apps/web/.storybook/preview.ts
+++ apps/web/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/svelte';
+import { viewports } from './viewports';
 
 const preview: Preview = {
 	parameters: {
@@ -8,6 +9,9 @@ const preview: Preview = {
 				color: /(background|color)$/i,
 				date: /Date$/
 			}
+		},
+		viewport: {
+			viewports
 		}
 	}
 };
