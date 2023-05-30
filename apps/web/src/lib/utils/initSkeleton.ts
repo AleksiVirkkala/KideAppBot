fc02c1b4d05d633b16553c9a827816f930023f24
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-30 16:51:10 +0300

    feat(web): :lipstick: Create custom theme

--- apps/web/src/lib/utils/initSkeleton.ts
+++ apps/web/src/lib/utils/initSkeleton.ts
@@ -1,5 +1,5 @@
 // The ordering of these imports is critical to your app working properly
-import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+import './theme.css';
 // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 import '@skeletonlabs/skeleton/styles/skeleton.css';
 // Most of your app wide CSS should be put in this file
