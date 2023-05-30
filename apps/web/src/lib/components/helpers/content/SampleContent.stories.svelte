f0004dbc49b6d71dfc22eefb51535f8256871861
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-30 16:53:17 +0300

    feat(web:ui): :technologist: Create SampleContent component

---
+++ apps/web/src/lib/components/helpers/content/SampleContent.stories.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import SampleContent from './SampleContent.svelte';
+</script>
+
+<Meta title="helpers/SampleContent" component={SampleContent} />
+
+<Template let:args>
+	<div class="w-[500px]">
+		<SampleContent {...args} class="h-12" />
+	</div>
+</Template>
+
+<Story name="Default" />
+
+<Story
+	name="WithoutStripes"
+	args={{
+		disableStripes: true
+	}}
+/>
+
+<Story name="WithCustomStyling">
+	<div class="w-[500px]">
+		<SampleContent class="h-72 rounded-none border-4 border-red-500 bg-slate-300" />
+	</div>
+</Story>
+
+<Story name="WithCustomContent">
+	<div class="w-[500px]">
+		<SampleContent>
+			<div class="flex h-full flex-col items-center justify-center">
+				<h1 class="text-3xl font-bold text-gray-900">Custom Content</h1>
+				<p class="text-gray-700">This is a custom content area</p>
+			</div>
+		</SampleContent>
+	</div>
+</Story>
---
+++ apps/web/src/lib/components/helpers/content/SampleContent.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { twMerge } from '$lib/utils/twMerge';
+
+	export let disableStripes = false;
+	export let label: string | undefined = undefined;
+	let className = '';
+	export { className as class };
+</script>
+
+<div
+	use:twMerge={className}
+	class="relative h-40 overflow-auto rounded-xl border border-dashed border-gray-400 opacity-75"
+>
+	{#if label}
+		<div class="absolute left-4 top-3 text-sm">{label}</div>
+	{/if}
+
+	{#if !disableStripes}
+		<svg class="absolute inset-0 h-full w-full stroke-gray-900/10" fill="none">
+			<defs>
+				<pattern
+					id="pattern-diagonal-lines"
+					x="0"
+					y="0"
+					width="10"
+					height="10"
+					patternUnits="userSpaceOnUse"
+				>
+					<path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
+				</pattern>
+			</defs>
+			<rect fill="url(#pattern-diagonal-lines)" class="h-full w-full stroke-none" />
+		</svg>
+	{/if}
+
+	<slot />
+</div>
