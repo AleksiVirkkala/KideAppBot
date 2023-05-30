4b89de8c684bc550d02f407f7ab99fc89d898acf
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-30 19:49:56 +0300

    feat(web): :construction: AppBar component

---
+++ apps/web/src/lib/components/application/AppBar.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import AppBar from './AppBar.svelte';
+</script>
+
+<Meta
+	title="application/AppBar"
+	component={AppBar}
+	parameters={{
+		layout: 'fullscreen'
+	}}
+/>
+
+<Template let:args>
+	<AppBar {...args} />
+</Template>
+
+<Story
+	name="Desktop"
+	parameters={{
+		viewport: {
+			defaultViewport: 'fullHD'
+		}
+	}}
+/>
+
+<Story
+	name="Mobile"
+	parameters={{
+		viewport: {
+			defaultViewport: 'iphone12'
+		}
+	}}
+/>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,44 +1,12 @@
 <script lang="ts">
+	import AppBar from '$lib/components/application/AppBar.svelte';
 	import '$lib/utils/initSkeleton';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<AppBar />
 	</svelte:fragment>
-	<!-- Page Route Content -->
 	<slot />
 </AppShell>
