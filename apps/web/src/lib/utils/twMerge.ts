9f23ed79a152e62d7a7259b78f323a66a5f41b1c
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-05-31 19:33:16 +0300

    feat(web): :technologist: twmerge class array support

--- apps/web/src/lib/utils/twMerge.ts
+++ apps/web/src/lib/utils/twMerge.ts
@@ -1,10 +1,12 @@
 import { twMerge as tailwindMerge } from 'tailwind-merge';
 
-export function twMerge(node: Element, className: string) {
+export function twMerge(node: Element, className: string | string[]) {
+	const classNames: string[] = Array.isArray(className) ? className : [className];
+
 	if (node instanceof SVGElement) {
 		// There is no `className` property on SVG elements.
-		node.setAttribute('class', tailwindMerge(node.getAttribute('class'), className));
+		node.setAttribute('class', tailwindMerge(node.getAttribute('class'), ...classNames));
 	} else {
-		node.className = tailwindMerge(node.className, className);
+		node.className = tailwindMerge(node.className, ...classNames);
 	}
 }
