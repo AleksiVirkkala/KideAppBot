777b5a1259235b8454beff2c72c8edd0a991d9ab
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-06-01 12:48:38 +0300

    feat(web:app): :children_crossing: Update theme-color based on app mode (dark/light)

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -3,8 +3,15 @@
 	import { AppBar, type NavigationOption } from '$lib/components/application/AppBar';
 	import { appDescription, appName } from '$lib/utils/appInfo';
 	import { appleSplashScreenMeta, appleTouchIconMeta } from '$lib/utils/metadata';
-	import autoAnimate from '@formkit/auto-animate';
 	import { page } from '$app/stores';
+	import autoAnimate from '@formkit/auto-animate';
+
+	import { modeCurrent } from '@skeletonlabs/skeleton';
+	import { derived } from 'svelte/store';
+
+	const themeColor = derived(modeCurrent, $modeCurrent =>
+		$modeCurrent ? 'rgb(237, 237, 238)' : 'rgb(66, 65, 71)'
+	);
 
 	const navOptions = [
 		{ label: 'Bot', href: '/bot' },
@@ -16,7 +23,7 @@
 	<title>{appName}</title>
 	<meta name="description" content={appDescription} />
 	<meta name="application-name" content={appName} />
-	<meta name="theme-color" content="#f3f4f6" />
+	<meta name="theme-color" content={$themeColor} />
 	<!-- Enable Apple PWA -->
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-title" content={appName} />
