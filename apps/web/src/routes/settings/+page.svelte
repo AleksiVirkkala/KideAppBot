ff9b1aa705b446b0e116dbd98e2eaa38d74f9e3b
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-06-04 15:52:13 +0300

    feat(web:app): :sparkles: Basic settings page where user can set kideapp token

--- apps/web/src/routes/settings/+page.svelte
+++ apps/web/src/routes/settings/+page.svelte
@@ -1,3 +1,11 @@
-<div class="flex grow flex-col justify-center text-center">
-	<span class="text-2xl">Settings.</span>
+<script>
+	import { token } from '$lib/stores/token';
+</script>
+
+<div class="flex grow flex-col space-y-4 pb-2 pt-4 sm:space-y-8 sm:pb-6 sm:pt-8">
+	<h2 class="h2">Settings</h2>
+	<p>Token will be saved to browser local storage and used for ticket reservation.</p>
+	<div class="flex space-x-4 sm:space-x-8">
+		<input type="password" class="input" required placeholder="Bearer token" bind:value={$token} />
+	</div>
 </div>
