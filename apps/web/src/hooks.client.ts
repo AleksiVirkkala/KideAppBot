572124df67beabae8087feca449ca92d92292ace
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-08-24 15:40:48 +0300

    feat(web:app): :technologist: Disable highlight on localhost

--- apps/web/src/hooks.client.ts
+++ apps/web/src/hooks.client.ts
@@ -2,12 +2,14 @@ import { PUBLIC_HIGHLIGHT_PROJECT_ID } from '$env/static/public';
 import { appVersion } from '$lib/utils/appInfo';
 import { H } from 'highlight.run';
 
-H.init(PUBLIC_HIGHLIGHT_PROJECT_ID, {
-	environment: 'production',
-	version: appVersion,
-	tracingOrigins: true,
-	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true
-	}
-});
+if (location.hostname !== 'localhost') {
+	H.init(PUBLIC_HIGHLIGHT_PROJECT_ID, {
+		environment: 'production',
+		version: appVersion,
+		tracingOrigins: true,
+		networkRecording: {
+			enabled: true,
+			recordHeadersAndBody: true
+		}
+	});
+}
